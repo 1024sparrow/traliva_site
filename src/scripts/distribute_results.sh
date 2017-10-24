21fda7af5161c1e3357c6f9b7861ef68e989424a
VasilyevBP <user@ald-1>
2017-10-24 11:45:21 +0300

    Сжатие css-ников, и вообще их наличие (вставил их в index.html).

--- src/scripts/distribute_results.sh
+++ src/scripts/distribute_results.sh
@@ -4,6 +4,10 @@
 #echo $2
 #touch $1/$2/123
 
+mkdir $1/static
+mkdir $1/templates
+rm -r $1/static/*
+rm -r $1/templates/*
 mv $1/$2/images/* $1/static/
 mv $1/$2/contrib $1/static/
 mv $1/$2/index/input_data/tab_apps $1/static/
--- src/scripts/tabs_meta/process_dir.js
+++ src/scripts/tabs_meta/process_dir.js
@@ -4,6 +4,10 @@ var process = require('process');
 var fs = require('fs');
 var path = require('path');
 var child_process = require('child_process');
+var StringDecoder = require('string_decoder').StringDecoder;
+var DECODER = new StringDecoder('utf8');
+
+console.log('%%%:'+process.argv);
 
 var dirpath = path.resolve(process.argv[2], process.argv[3]);
 var filepath = path.resolve(dirpath, 'src_header.json');
@@ -32,15 +36,15 @@ fs.writeFileSync(path.resolve(dirpath, 'html_names'), JSON.stringify(html_names)
 fs.writeFileSync(path.resolve(dirpath, 'tabs_descr'), JSON.stringify(tabs_descr), 'utf8');
 
 //---- Объединение css-ников и их сжатие ----
-//boris here: Перед чтением запустить скрипт, сжимающий тот файл
+compress_css(path.resolve(dirpath, 'headerlevel_style.css'));
 var style = fs.readFileSync(path.resolve(dirpath, 'headerlevel_style.css'));
 // После чтения того файла, мы должны его удалить.
 child_process.execSync(`rm ${path.resolve(dirpath, 'headerlevel_style.css')}`);
-//boris here: Перед чтением запустить скрипт, сжимающий тот файл
+compress_css(path.resolve(dirpath, 'tab_apps', 'style.css'));
 style += fs.readFileSync(path.resolve(dirpath, 'tab_apps', 'style.css'));
 child_process.execSync(`rm ${path.resolve(dirpath, 'tab_apps', 'style.css')}`);
 for (const tabObj of h.tabs){
-    //boris here: Перед чтением запустить скрипт, сжимающий тот файл
+    compress_css(path.resolve(dirpath, 'tab_apps', tabObj.id, 'style.css'), tabObj.id);
     style += fs.readFileSync(path.resolve(dirpath, 'tab_apps', tabObj.id, 'style.css'));
     //boris here: После чтения того файла, мы должны его удалить.
     child_process.execSync(`rm ${path.resolve(dirpath, 'tab_apps', tabObj.id, 'style.css')}`);
@@ -49,3 +53,15 @@ fs.writeFileSync(path.resolve(dirpath, 'style.css'), style, 'utf8');
 
 
 child_process.execSync(`rm ${path.resolve(filepath)}`);
+
+
+
+function compress_css(filepath, prefix){
+    const processScriptPath = path.resolve(path.dirname(process.argv[1]), 'css.sh');
+    let tt = `${processScriptPath} ${filepath}`;
+    if (prefix)
+        tt += ` ${prefix}`;
+    const ou = child_process.execSync(tt, {encoding:'utf8', stdio:[0,1,2]});
+    if (ou)
+        console.log(DECODER.write(ou));
+}
