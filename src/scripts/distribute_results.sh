796df18c4af59b223db41dde64bebc07113c835a
boris <1024sparrow@gmail.com>
2017-10-28 12:39:59 +0300

    Сделано копирование static_files в static.

--- src/scripts/distribute_results.sh
+++ src/scripts/distribute_results.sh
@@ -13,4 +13,11 @@ cp -r $1/$2/contrib $1/static/
 mv $1/$2/index/input_data/tab_apps $1/static/
 mv $1/$2/index/gameplay.js $1/templates/
 mv $1/$2/index/index.html $1/templates/
+
+mkdir $1/static/tabs
+for i in $(ls -d $1/src/index/input_data/tab_apps/*/ | sed -e's/\/$//' -e's/.*\///' | grep -v ^_);
+do
+    cp -rf $1/src/index/input_data/tab_apps/$i/static_files $1/static/tabs/$i
+done
+
 rm -r $1/$2
