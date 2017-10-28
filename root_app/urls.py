b102c34ab7f795f7865ac41b03be9d74f0e51b54
boris <1024sparrow@gmail.com>
2017-10-28 17:35:20 +0300

    Скорректирован django-вский файлик 'urls.py' в соответствии с используемым StateToUriMapper-ом.

--- root_app/urls.py
+++ root_app/urls.py
@@ -3,12 +3,10 @@ from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
-    #rl(r'^html/(?P<tab_id>[^/]+)/$', views.view_html),
+    #url(r'^html/(?P<tab_id>[^/]+)/$', views.view_html),
 
-    #url(r'^$', RedirectView.as_view(url='/' + header_meta['tabs'][0]['id'] + '/')),
     url(r'^$', RedirectView.as_view(url='/root/')),
-    url(r'^(?P<tab_id>[^/]+)/$', views.view_html),
-    url(r'^(?P<tab_id>[^/]+)/gameplay.js$', views.gameplay_js),
-    url(r'^(?P<tab_id>[^/]+)/html/(?P<html_id>[^/]+)$', views.gameplay_html),
-    #url(r'^/(?P<tab_id>[^/]+).js$', views.view_js),
+    url(r'^(?P<tab_id>[^/]+)(.*)/gameplay.js$', views.gameplay_js),
+    url(r'^(?P<tab_id>[^/]+)(.*)/html/(?P<html_id>[^/]+)$', views.gameplay_html),
+    url(r'^(?P<tab_id>[^/]+)/', views.view_html),
 ]
